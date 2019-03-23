f93094a02c731acacc88d9f467283c5f0486dd50
Salun Marvin Soares <salunmarvin@gmail.com>
2019-03-23 12:57:54 -0300

    master - added db connection

--- server/secrets.js
+++ server/secrets.js
@@ -1,5 +1,5 @@
 const secrets = {
-  dbUri: process.env.DB_URI || 'YOUR MONGODB URI HERE',
+  dbUri: process.env.DB_URI || 'mongodb://loungemgmt:b2a5r67@ds015909.mlab.com:15909/loungemgmt',
 };
 
 const getSecret = (key) => secrets[key];
