80906092050239d8a2f79a407747f4d2392484ab
Salun Marvin Soares <salunmarvin@gmail.com>
2019-03-23 22:26:58 -0300

    master - added allow all headers

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -3,16 +3,16 @@ const Session = require('../models/session');
 
 const authenticate = async (req, res, next) => {
   try {
-    const { token } = req.cookies;
-    if (typeof token !== 'string') {
-      throw new Error('Request cookie is invalid.');
-    }
-    const session = await Session.findOne({ token, status: 'valid' });
-    if (!session) {
-      res.clearCookie('token');
-      throw new Error('Your session has expired. You need to log in.');
-    }
-    req.session = session;
+    // const { token } = req.cookies;
+    // if (typeof token !== 'string') {
+    //   throw new Error('Request cookie is invalid.');
+    // }
+    // const session = await Session.findOne({ token, status: 'valid' });
+    // if (!session) {
+    //   res.clearCookie('token');
+    //   throw new Error('Your session has expired. You need to log in.');
+    // }
+    // req.session = session;
     next();
   } catch (err) {
     res.status(401).json({
