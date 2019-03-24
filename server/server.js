eb6ca7fbd9e9cb353777af703409c9677e38b16a
Salun Marvin Soares <salunmarvin@gmail.com>
2019-03-23 22:28:30 -0300

    master - added allow all headers

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ const port = process.env.PORT || 3000;
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", '*');
   res.header("Access-Control-Allow-Credentials", true);
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,HEAD,OPTIONS');
   res.header("Access-Control-Allow-Headers", '*');
   next();
 });
