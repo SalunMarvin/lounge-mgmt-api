496bc23ad81ef8cb7687e97bed59f3fb4e5038d0
Salun Marvin Soares <salunmarvin@gmail.com>
2019-03-24 17:59:07 -0300

    master - client crud

---
+++ server/models/client.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const ClientSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: false,
+        trim: true,
+        unique: false,
+    },
+    uniqueNumber: {
+        type: Number,
+        required: false,
+        trim: true,
+        unique: true,
+    },
+    birthDate: {
+        type: Date,
+        required: false,
+        trim: true,
+        unique: false,
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true,
+    },
+});
+
+ClientSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model('Client', ClientSchema);
---
+++ server/routes/clients.js
@@ -0,0 +1,75 @@
+const express = require('express');
+
+const Client = require('../models/client');
+const {
+    authenticate
+} = require('../middleware/authenticate');
+
+const router = express.Router();
+
+router.post('/', authenticate, async (req, res) => {
+    try {
+        const client = new Client(req.body);
+        const persistedClient = await client.save();
+
+        res
+            .status(201)
+            .json({
+                title: 'Client Registration Successful',
+                detail: 'Successfully registered new client',
+                persistedClient
+            });
+    } catch (err) {
+        res.status(400).json({
+            errors: [{
+                title: 'Client Registration Error',
+                detail: 'Something went wrong during client registration process.',
+                errorMessage: err.message,
+            }, ],
+        });
+    }
+});
+
+router.get('/', authenticate, async (req, res) => {
+    try {
+        const clients = await Client.find({});
+
+        res.json({
+            title: 'Successful operation',
+            detail: 'Successfully got all clients',
+            clients,
+        });
+    } catch (err) {
+        res.status(401).json({
+            errors: [{
+                title: 'Unauthorized',
+                detail: 'Not authorized to access this route',
+                errorMessage: err.message,
+            }, ],
+        });
+    }
+});
+
+router.post('/search', authenticate, async (req, res) => {
+    try {
+        const { name } = req.body;
+        const clients = await Client.find({ name: { "$regex": name, "$options": "i" } });
+
+        res.json({
+            title: 'Successful operation',
+            detail: 'Successfully got all products',
+            clients,
+        });
+    } catch (err) {
+        res.status(401).json({
+            errors: [{
+                title: 'Unauthorized',
+                detail: 'Not authorized to access this route',
+                errorMessage: err.message,
+            }, ],
+        });
+    }
+});
+
+
+module.exports = router;
