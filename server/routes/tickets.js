6137eb45e778931069ee03fbffc1c2806b4980c3
Salun Marvin Soares <salunmarvin@gmail.com>
2019-03-24 17:41:28 -0300

    master - delete product from ticket

--- server/routes/tickets.js
+++ server/routes/tickets.js
@@ -101,6 +101,65 @@ router.post('/product', authenticate, async (req, res) => {
     }
 });
 
+router.delete('/product', authenticate, async (req, res) => {
+    try {
+        const {
+            productId,
+            ticketId,
+        } = req.body;
+        
+        let ticket = await Ticket.findById({ _id: ticketId });
+        let product = await Product.findOne({ _id: productId });
+
+        ticket.totalPrice = ticket.totalPrice - product.price;
+        let index = ticket.products.findIndex(criteria => criteria = product._id)
+        ticket.products.splice(index, 1)
+        let persistedTicket = await ticket.save();
+        persistedTicket = await Ticket.findOne({ _id: persistedTicket._id }).populate('products')
+
+        let products = []
+        persistedTicket.products.map(product => {
+            if (!products.some(item => item._id === product._id)) {
+                let newProduct = product;
+                product.quantity = 1;
+                products.push(newProduct)
+            } else {
+                let arrayProduct = products.find(criteria => criteria._id === product._id)
+                let newProduct = {
+                    _id: arrayProduct._id,
+                    name: arrayProduct.name,
+                    barCode: arrayProduct.barCode,
+                    quantity: arrayProduct.quantity + 1,
+                    price: arrayProduct.price + arrayProduct.price,
+                    uniqueCode: arrayProduct.uniqueCode,
+                }
+
+                let newArray = [arrayProduct];
+                products = products.filter(item => !newArray.includes(item))
+
+                products.push(newProduct);
+            }
+        })
+        persistedTicket.products = products
+
+        res
+            .status(201)
+            .json({
+                title: 'OK',
+                detail: 'Produto removido com sucesso',
+                persistedTicket
+            });
+    } catch (err) {
+        res.status(400).json({
+            errors: [{
+                title: 'Erro',
+                detail: 'Não foi possível remover um novo produto',
+                errorMessage: err.message,
+            }, ],
+        });
+    }
+});
+
 router.get('/', authenticate, async (req, res) => {
     try {
         const tickets = await Ticket.find({});
