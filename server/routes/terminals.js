60af7f89915060bedc5937269e8944b5c0b3ac5d
Salun Marvin Soares <salunmarvin@gmail.com>
2019-04-07 22:22:15 -0300

    master

--- server/routes/terminals.js
+++ server/routes/terminals.js
@@ -27,4 +27,24 @@ router.get('/', authenticate, async (req, res) => {
     }
 });
 
+router.get('/:id', authenticate, async (req, res) => {
+    try {
+        const terminal = await Terminal.findById(req.params.id);
+
+        res.json({
+            title: 'OK',
+            detail: 'Terminal encontrado com sucesso',
+            terminal,
+        });
+    } catch (err) {
+        res.status(401).json({
+            errors: [{
+                title: 'Unauthorized',
+                detail: 'Not authorized to access this route',
+                errorMessage: err.message,
+            }, ],
+        });
+    }
+});
+
 module.exports = router;
