4c33c5794932ec544c5d4087da521a7b3420ca70
Salun Marvin Soares <salunmarvin@gmail.com>
2019-04-19 16:24:37 -0300

    master

--- server/routes/orders.js
+++ server/routes/orders.js
@@ -29,7 +29,7 @@ router.get('/', authenticate, async (req, res) => {
 
 router.get('/terminal/:id', authenticate, async (req, res) => {
     try {
-        const orders = await Order.find({ terminal: req.params.id }).sort({'created': 1});
+        const orders = await Order.find({ terminal: req.params.id, ready: false }).sort({'created': 1});
 
         res.json({
             title: 'Successful operation',
