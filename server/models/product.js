c3a5715d554f344961ee0210288378bcc0ba98fc
Salun Marvin Soares <salunmarvin@gmail.com>
2019-05-07 19:04:17 -0300

    master

--- server/models/product.js
+++ server/models/product.js
@@ -19,7 +19,7 @@ const ProductSchema = new mongoose.Schema({
         type: String,
         required: false,
         trim: true,
-        unique: true,
+        unique: false,
     },
     quantity: {
         type: Number,
